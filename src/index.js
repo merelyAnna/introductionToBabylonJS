584ce0e875571e55f9b726b80971e500459116c7
Anna <anna@MacBook.local>
2019-03-21 19:43:40 +0100

    Basic Scene with a Box

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Engine, Scene } from "babylonjs";
+import { Engine, Scene, MeshBuilder, StandardMaterial, Color4, Vector3, ArcRotateCamera, HemisphericLight } from "babylonjs";
 
 const initGame = () => {
     const canvas = document.getElementById("babylon-canvas");
@@ -6,8 +6,40 @@ const initGame = () => {
     const engine = new Engine(canvas, true);
     const scene = new Scene(engine);
 
+    const light = new HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 10, 0),
+        scene
+    );
+
+    const camera = new ArcRotateCamera(
+        "camera",
+        0,
+        Math.PI / 3,
+        10.0,
+        new BABYLON.Vector3(0, 10, -30),
+        scene
+    );
+    camera.attachControl(canvas, true);
+    camera.setTarget(new Vector3(0, 0, 0));
+
+    const material = new StandardMaterial("material");
+    material.diffuseColor = new Color4(0.22, 0.89, 0.76, 1);
+    material.specularColor = new Color4(0, 0.5, 0.76, 1);
+
+    const mesh = MeshBuilder.CreateBox(
+        "mesh",
+        { width: 10, height: 10, depth: 10 },
+        scene
+    );
+
+    mesh.material = material;
+
     engine.runRenderLoop(function() {
         scene.render();
+
     });
+
 };
+
 initGame();
