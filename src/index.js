2186225b981aa98574fa2ce4378a41b36b94de18
merelyAnna <anna.k.backs@gmail.com>
2019-04-10 11:04:09 +0200

    Restructuring

--- src/index.js
+++ src/index.js
@@ -14,13 +14,53 @@ import {
   Texture
 } from "babylonjs";
 
+const createBox = scene => {
+
+    //Creating the box mesh
+    const box = MeshBuilder.CreateBox(
+        "box",
+        { width: 10, height: 10, depth: 10 },
+        scene
+    );
+
+    // Positioning the box
+    box.position = new Vector3(2, 3, 4);
+
+    // Re-Positioning the box
+    box.position.x = 3;
+    box.position.y = 2;
+    box.position.z = 1;
+
+    // Rotating the box
+    box.rotation = new Vector3(2, 3, 4);
+
+    // Resetting the rotation of the box
+    box.rotation.x = Math.PI / 9;
+    box.rotation.y = Math.PI / 8;
+    box.rotation.z = Math.PI / 7;
+
+    // Creating the material
+    const material = new StandardMaterial("material");
+    material.diffuseColor = new Color3(0.22, 0.89, 0.76);
+    material.specularColor = new Color3(0, 0.5, 0.76);
+
+    // Adding an action
+    box.actionManager = new ActionManager(scene);
+    box.actionManager.registerAction(
+        new ExecuteCodeAction(ActionManager.OnPickTrigger, () => {
+            console.log("Clicked on box");
+            box.material = material;
+        })
+    );
+}
+
 const createDuckParticleSystem = scene => {
   const duckParticleSystem = new ParticleSystem("dugs", 2000, scene);
   duckParticleSystem.particleTexture = new Texture("/assets/duck.png", scene);
   duckParticleSystem.emitter = new Vector3(0, 20, 0);
   duckParticleSystem.minEmitBox = new Vector3(-20, -20, -20);
   duckParticleSystem.maxEmitBox = new Vector3(20, 20, 20);
-  duckParticleSystem.gravity = new BABYLON.Vector3(0, -10, 0);
+  duckParticleSystem.gravity = new Vector3(0, -10, 0);
   duckParticleSystem.emitRate = 999;
   duckParticleSystem.start();
 };
@@ -29,7 +69,7 @@ const physics = scene => {
   scene.enablePhysics();
   sphere.physicsImpostor = new PhysicsImpostor(
     sphere,
-    BABYLON.PhysicsImpostor.SphereImpostor,
+    PhysicsImpostor.SphereImpostor,
     { mass: 1, restitution: 0.9 },
     scene
   );
@@ -54,42 +94,7 @@ const initGame = () => {
   camera.attachControl(canvas, true);
   camera.setTarget(new Vector3(0, 0, 0));
 
-  //Creating the box mesh
-  const box = MeshBuilder.CreateBox(
-    "box",
-    { width: 10, height: 10, depth: 10 },
-    scene
-  );
-
-  // Positioning the box
-  box.position = new Vector3(2, 3, 4);
-
-  // Re-Positioning the box
-  box.position.x = 3;
-  box.position.y = 2;
-  box.position.z = 1;
-
-  // Rotating the box
-  box.rotation = new Vector3(2, 3, 4);
-
-  // Resetting the rotation of the box
-  box.rotation.x = Math.PI / 9;
-  box.rotation.y = Math.PI / 8;
-  box.rotation.z = Math.PI / 7;
-
-  // Creating the material
-  const material = new StandardMaterial("material");
-  material.diffuseColor = new Color3(0.22, 0.89, 0.76);
-  material.specularColor = new Color3(0, 0.5, 0.76);
-
-  // Adding an action
-  box.actionManager = new ActionManager(scene);
-  box.actionManager.registerAction(
-    new ExecuteCodeAction(ActionManager.OnPickTrigger, () => {
-      console.log("Clicked on box");
-      box.material = material;
-    })
-  );
+  createBox(scene);
 
   createDuckParticleSystem(scene);
 
